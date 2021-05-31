a964f9271716310ddaa8c76d87e647ae0594e245
Pedro Castro <pecastro@pagseguro.com>
2021-05-30 21:19:45 -0300

    Initial commit

---
+++ src/main/kotlin/com/castrocorp/budgetcontrol/BudgetControlApplication.kt
@@ -0,0 +1,11 @@
+package com.castrocorp.budgetcontrol
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BudgetControlApplication
+
+fun main(args: Array<String>) {
+	runApplication<BudgetControlApplication>(*args)
+}
